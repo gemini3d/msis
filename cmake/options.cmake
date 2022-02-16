e96706ae0dfec763f1fccc65f9246222d616e8e8
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-16 13:08:03 -0500

    install Rpath necessary for shared install from user programs

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,14 +2,15 @@ option(msis2 "use MSIS 2.x neutral atmosphere model")
 
 set(CMAKE_TLS_VERIFY true)  # for Git and Downloads
 
+# Rpath options necessary for shared library install to work correctly in user projects
+set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/lib)
+set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/lib)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
+
 # to make more usable by external programs, put all Fortran .mod generated module files in a single directory.
 set(CMAKE_Fortran_MODULE_DIRECTORY ${PROJECT_BINARY_DIR}/include)
 
-if(dev)
-
-else()
-  set(FETCHCONTENT_UPDATES_DISCONNECTED_MSIS2 true)
-endif()
+set(FETCHCONTENT_UPDATES_DISCONNECTED_MSIS2 true)
 
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
