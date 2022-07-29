13ff8a8c59ce86ee384c4860f48ca0ae2ed3cf6a
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-29 15:28:11 -0400

    correct linking with HDF5-MPI

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,8 @@
+message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION}  CMake ${CMAKE_VERSION}")
+
 option(msis2 "use MSIS 2.x neutral atmosphere model")
 
-set(CMAKE_TLS_VERIFY true)  # for Git and Downloads
+set(CMAKE_TLS_VERIFY true)
 
 # Rpath options necessary for shared library install to work correctly in user projects
 set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/lib)
@@ -12,13 +14,18 @@ set(CMAKE_Fortran_MODULE_DIRECTORY ${PROJECT_BINARY_DIR}/include)
 
 set(FETCHCONTENT_UPDATES_DISCONNECTED true)
 
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  # CMAKE_BINARY_DIR for use from FetchContent
+  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
+endif()
+
+# allow CMAKE_PREFIX_PATH with ~ expand
+if(CMAKE_PREFIX_PATH)
+  get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
+endif()
+
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
-endif()
-
-# --- default install directory under build/local
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-  # will not take effect without FORCE
-  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
 endif()
