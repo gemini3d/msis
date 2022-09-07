3adc09627e23bd02730fa73c3d56058f912a2bc9
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-07 14:26:05 -0400

    patch reference simplify [skip ci]

--- cmake/PatchFile.cmake
+++ cmake/PatchFile.cmake
@@ -1,31 +1,13 @@
 # use GNU Patch from any platform
 
-find_program(PATCH NAMES patch)
+find_package(Patch REQUIRED)
 
-if(WIN32 AND NOT PATCH)
-  find_package(Git)
-  if(Git_FOUND)
-    get_filename_component(GIT_DIR ${GIT_EXECUTABLE} DIRECTORY)
-    get_filename_component(GIT_DIR ${GIT_DIR} DIRECTORY)
-
-    find_program(PATCH
-    NAMES patch
-    HINTS ${GIT_DIR}
-    PATH_SUFFIXES usr/bin
-    )
-  endif()
-endif()
-
-if(NOT PATCH)
-  message(FATAL_ERROR "Did not find GNU PATCH")
-endif()
-
-execute_process(COMMAND ${PATCH} ${in_file} --input=${patch_file} --output=${out_file} --ignore-whitespace
-TIMEOUT 15
+execute_process(COMMAND ${Patch_EXECUTABLE} ${in_file} --input=${patch_file} --output=${out_file} --ignore-whitespace
 COMMAND_ECHO STDOUT
 RESULT_VARIABLE ret
 )
 
 if(NOT ret EQUAL 0)
-  message(FATAL_ERROR "Failed to apply patch ${patch_file} to ${in_file} with ${PATCH}")
+  message(FATAL_ERROR "Failed to apply patch ${patch_file} to ${in_file} with ${Patch_EXECUTABLE}:
+  ${ret}")
 endif()
