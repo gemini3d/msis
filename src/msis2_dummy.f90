b4312908370fe85a3761e19e9a60795ab6b211dc
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-23 19:36:38 -0400

    error stop 2 if MSIS 2 not enabled

--- src/msis2_dummy.f90
+++ src/msis2_dummy.f90
@@ -10,7 +10,7 @@ subroutine msiscalc(day,utsec,z,lat,lon,sfluxavg,sflux,ap,tn,dn,tex)
 class(*) :: day,utsec,z,lat,lon,sfluxavg,sflux,ap(7), tn, dn(10), tex
 
 write(stderr,*) 'ERROR: to use MSIS 2.x requires "cmake -Dmsis2=yes"'
-error stop 20
+error stop 2
 
 end subroutine msiscalc
 
@@ -37,7 +37,7 @@ real(real32), optional :: switch_legacy(25)
 logical, optional :: lzalt_type,lspec_select(nspec-1), lmass_include(nspec-1), lN2_msis00
 
 write(stderr,*) 'ERROR: to use MSIS 2.x requires "cmake -B build -Dmsis2=yes"'
-error stop 20
+error stop 2
 
 end subroutine msisinit
 
