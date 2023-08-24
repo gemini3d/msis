45cb7fac4cc434b07bae46b9177cbb65b8a6965c
scivision <scivision@users.noreply.github.com>
2023-08-24 14:02:06 -0400

    source needs legacy flag

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,3 +1,4 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-Werror=line-truncation>)
+  set(legacy_flags $<$<COMPILE_LANGUAGE:Fortran>:-std=legacy>)
 endif()
