a23a174edf8a197cd794215297a9d3105ab5ae1d
scivision <scivision@users.noreply.github.com>
2024-01-18 23:29:48 -0500

    git submodule h5fortran

--- options.cmake
+++ options.cmake
@@ -2,27 +2,15 @@ message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} Toolch
 
 option(msis2 "use MSIS 2.x neutral atmosphere model" on)
 
-option(${PROJECT_NAME}_BUILD_TESTING "build test programs" on)
+option(${PROJECT_NAME}_BUILD_TESTING "build test programs" ${PROJECT_IS_TOP_LEVEL})
 
 option(${PROJECT_NAME}_BUILD_UTILS "build msis_setup" on)
 
-set(CMAKE_TLS_VERIFY true)
-
 include(GNUInstallDirs)
 
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-  set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE PATH "default install path" FORCE)
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND PROJECT_IS_TOP_LEVEL)
+  set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "default install path" FORCE)
 endif()
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
-
-# to make more usable by external programs, put all Fortran .mod generated module files in a single directory.
-set(CMAKE_Fortran_MODULE_DIRECTORY ${PROJECT_BINARY_DIR}/include)
-
-set(FETCHCONTENT_UPDATES_DISCONNECTED true)
-
-# allow CMAKE_PREFIX_PATH with ~ expand
-if(CMAKE_PREFIX_PATH)
-  get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
-endif()
