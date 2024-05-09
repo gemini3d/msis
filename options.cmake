7b83336b03cba2861a601308b05fd362b98f77e7
scivision <scivision@users.noreply.github.com>
2024-05-09 17:14:45 -0400

    CMake: don't print configure summary, it's confusing

--- options.cmake
+++ options.cmake
@@ -1,16 +1,10 @@
 message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
 
-option(msis2 "use MSIS 2.x neutral atmosphere model" on)
-
 option(${PROJECT_NAME}_BUILD_TESTING "build test programs" ${PROJECT_IS_TOP_LEVEL})
 
 option(${PROJECT_NAME}_BUILD_UTILS "build msis_setup" on)
 
-include(GNUInstallDirs)
-
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND PROJECT_IS_TOP_LEVEL)
-  set_property(CACHE CMAKE_INSTALL_PREFIX PROPERTY VALUE "${PROJECT_BINARY_DIR}/local")
-endif()
+option(msis2 "use MSIS 2.x neutral atmosphere model" on)
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
