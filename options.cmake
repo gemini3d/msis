796522fb960b45377fb7ae4719f9b2bda5b8e447
scivision <scivision@users.noreply.github.com>
2024-05-09 17:17:58 -0400

    CMake < 3.21 needs PROJECT_IS_TOP_LEVEL defined to avoid error

--- options.cmake
+++ options.cmake
@@ -1,5 +1,13 @@
 message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
 
+# this is NECESSARY for CMake 3.21 -- project will error on CMake configure!
+if(CMAKE_VERSION VERSION_LESS 3.21)
+  get_property(not_top DIRECTORY PROPERTY PARENT_DIRECTORY)
+  if(NOT not_top)
+    set(PROJECT_IS_TOP_LEVEL true)
+ endif()
+endif()
+
 option(${PROJECT_NAME}_BUILD_TESTING "build test programs" ${PROJECT_IS_TOP_LEVEL})
 
 option(${PROJECT_NAME}_BUILD_UTILS "build msis_setup" on)
