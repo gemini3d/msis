67150facf18d534156f78498b93ae70363429709
scivision <scivision@users.noreply.github.com>
2024-07-11 18:07:09 -0400

    handle parent project better

--- options.cmake
+++ options.cmake
@@ -1,18 +1,8 @@
 message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
 
-# this is NECESSARY for CMake 3.21 -- project will error on CMake configure!
-if(CMAKE_VERSION VERSION_LESS 3.21)
-  get_property(not_top DIRECTORY PROPERTY PARENT_DIRECTORY)
-  if(not_top)
-    set(${PROJECT_NAME}_IS_TOP_LEVEL false)
-  else()
-    set(${PROJECT_NAME}_IS_TOP_LEVEL true)
-  endif()
-endif()
+option(msis_BUILD_TESTING "build test programs" ${msis_IS_TOP_LEVEL})
 
-option(${PROJECT_NAME}_BUILD_TESTING "build test programs" ${${PROJECT_NAME}_IS_TOP_LEVEL})
-
-option(${PROJECT_NAME}_BUILD_UTILS "build msis_setup" on)
+option(msis_BUILD_UTILS "build msis_setup" on)
 
 option(msis2 "use MSIS 2.x neutral atmosphere model" on)
 
